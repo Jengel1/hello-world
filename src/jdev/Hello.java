4482036b206289e3e5f7c80f21461a76048af681
Engelkemier <engelkja@vertafore.com>
2018-10-31 10:35:17 -0600

    add always to message

--- src/jdev/Hello.java
+++ src/jdev/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
   public static void main(String[] args) {
-    System.out.println("jdev says, Hello World");
+    System.out.println("jdev always says, Hello World");
   }
 }
